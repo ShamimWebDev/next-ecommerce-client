4ed3561cead5aaf2c670524da5fb1f103af93a9a
shamimwebdev <shamim.ct17@gmail.com>
2025-11-26 11:05:19 +0600

    image loading fixed 5nd times

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["freepik.com", "unsplash.com", "pixabay.com"],
+  },
 };
 
 export default nextConfig;
+
+
+
+
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
